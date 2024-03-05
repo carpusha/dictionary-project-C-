857ab5cf81822d7cf12e04a8032cd562e93cc96d
carpusha <blerdlol123@gmail.com>
2024-03-05 18:15:59 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+List<int> list = new List<int> { 1, 2, 3 };
+Dictionary<int, string> dictionary = new Dictionary<int, string>()
+{
+            { 1, "banana" },
+            { 2, "apple" },
+            { 3, "cucumber" },
+};
+Dictionary<string, int> newDictionary = new Dictionary<string, int>();
+foreach (var item in dictionary)
+{
+    newDictionary.Add(item.Value, item.Key);
+}
+
+string json = JsonSerializer.Serialize(newDictionary);
+Console.WriteLine(json);
+
+
+Console.ReadKey();
